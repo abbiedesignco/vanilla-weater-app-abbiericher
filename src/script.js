8448ab00d6bde880ab8f02b0931bb91a9f3f6ab1
abbiedesignco <abbier.design@gmail.com>
2023-10-22 16:11:43 -0400

    Added the relevant icon Api

--- src/script.js
+++ src/script.js
@@ -22,22 +22,29 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#currentTemperature");
   let cityElement = document.querySelector("#currentCity");
   let descriptionElement = document.querySelector("#currentDescription");
   let humidityElement = document.querySelector("#currentHumidity");
   let windElement = document.querySelector("#currentWind");
   let dateElement = document.querySelector("#currentDate");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement = response.data.weather[0].description;
   humidityElement = response.data.main.humidity;
   windElement = Math.round(response.data.wind.speed);
   dateElement = formatDate(response.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
 q=Montreal&appid=${apiKey}&units=metric`;
 
