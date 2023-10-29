236f961ba5ad37b8b57334a97a6da2bced12a630
abbiedesignco <abbier.design@gmail.com>
2023-10-29 17:20:54 -0400

    Changed the search function HTML

--- src/script.js
+++ src/script.js
@@ -99,8 +99,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=Montreal&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
